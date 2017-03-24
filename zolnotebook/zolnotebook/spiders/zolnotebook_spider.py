aed48bc28c16591cd36f6b696d33efef06613313
wk1024105222 <1024105222@qq.com>
2017-03-25 00:11:39 +0800

    使用非递归爬虫 测试通过
    1、根据查询结果 获取型号id
    2、生成目标 url
    3、 parse 中 yield 发起Request   回调函数使用parse_item
    4、parse_item 解析参数明细

--- zolnotebook/zolnotebook/spiders/zolnotebook_spider.py
+++ zolnotebook/zolnotebook/spiders/zolnotebook_spider.py
@@ -11,35 +11,29 @@ class ZolNoteBookCrawlerSpider(Spider):
 
     name = 'zolnotebook_spider'
     allowed_domains = ['http://detail.zol.com.cn/']
-    start_urls = \
-        ['http://detail.zol.com.cn/389/388392/param.shtml']
-            # 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s7236_1_2_0_1.html']#+\
-                 # [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s6974_1_2_0_'+str(i) for i in range(1,19,1)+'.html']+\
-                 # [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s6456_1_2_0_'+str(i) for i in range(1,23,1)+'.html']+\
-                 # [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s6316_1_2_0_'+str(i) for i in range(1,32,1)+'.html']+\
-                 # [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s6133_1_2_0_'+str(i) for i in range(1,24,1)+'.html']+\
-                 # [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s5719_1_2_0_'+str(i) for i in range(1,76,1)+'.html']
-
-    # def parse(self, response):
-    #     ids =  response.xpath('//li[@data-follow-id]/@data-follow-id').extract()
-    #     for a in ids:
-    #         id = a[1:]
-    #         page = int(id) /1000+1
-    #         url = 'http://detail.zol.com.cn/'+str(page)+'/'+str(id)+'/param.shtml'
-    #         # print  url
-    #         yield Request(url, callback=self.parse_item, dont_filter=True)
+    start_urls =    [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s7236_1_2_0_'+str(i)+'.html' for i in range(1, 8,1)]+\
+                    [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s6974_1_2_0_'+str(i)+'.html' for i in range(1,19,1)]+\
+                    [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s6456_1_2_0_'+str(i)+'.html' for i in range(1,23,1)]+\
+                    [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s6316_1_2_0_'+str(i)+'.html' for i in range(1,32,1)]+\
+                    [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s6133_1_2_0_'+str(i)+'.html' for i in range(1,24,1)]+\
+                    [ 'http://detail.zol.com.cn/notebook_index/subcate16_0_list_1_s5719_1_2_0_'+str(i)+'.html' for i in range(1,76,1)]
 
     def parse(self, response):
-        print response.url
-        item = ZolnotebookItem()
+        ids =  response.xpath('//li[@data-follow-id]/@data-follow-id').extract()
+        for a in ids:
+            id = a[1:]
+            page = int(id) /1000+1
+            url = 'http://detail.zol.com.cn/'+str(page)+'/'+str(id)+'/param.shtml'
+            # print  url
+            yield Request(url, callback=self.parse_item, dont_filter=True)
 
-        list = []
+    def parse_item(self, response):
+        item = ZolnotebookItem()
 
         num = [tmp.split('_')[1] for tmp in response.xpath('//span[@class="param-name"]/@id').extract()]+\
               [tmp.split('_')[1] for tmp in response.xpath('//span[@class="param-name param-explain"]/@id').extract()]
         map = {}
-        file_object = open(r'thefile.txt', 'w')
-
+        # file_object = open(r'thefile.txt', 'w')
         for i in num:
             name = response.xpath('//span[@id="newPmName_'+i+'"]/text()').extract()
             name_len=len(name)
@@ -62,20 +56,22 @@ class ZolNoteBookCrawlerSpider(Spider):
                     value = '|'.join(value)
 
             a = name.encode('utf-8')
-            b = value.encode('utf-8').replace('\r','').replace('\n','').replace(',','')
-            file_object.write(a+'=====')
-            file_object.write(b+'\n')
+            b = value.encode('utf-8')
+            # file_object.write(a+'=====')
+            # file_object.write(b+'\n')
             map[a] = b.replace('\r','').replace('\n','').replace(',','')
             # map[name] = value.encode("utf-8").replace('\r','').replace('\n','').replace(',','')
 
-        file_object.close( )
-
-        item['html']= ''
-        item['param']=map
-        item['url'] = response.url
         try:
-            item['name'] = response.xpath('//a[@class="back_home_btn"]/span/text()').extract()[0][2:-2]
+            name = response.xpath('//a[@class="back_home_btn"]/span/text()').extract()[0][2:-2]
         except Exception as e:
-            item['name']='error'
+            name='error'
 
+        map['url'] = response.url
+        map['name'] = name
+        item['param']=map
+        # file_object.write(response.url+'\n')
+        # file_object.write(item['name'].encode('utf-8')+'\n')
+        # print response.url, '================================================================================'
+        # file_object.close( )
         return item
