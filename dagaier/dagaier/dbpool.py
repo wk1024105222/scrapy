8ad92353941982815192ea9b37c49ae7ef283c57
wk1024105222 <1024105222@qq.com>
2018-07-03 00:21:35 +0800

    优化3

--- dagaier/dagaier/dbpool.py
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-import cx_Oracle
-from DBUtils.PooledDB import PooledDB
-
-pool = PooledDB(cx_Oracle, user = "wkai", password = "wkai", dsn = "127.0.0.1:1521/wkai",mincached=2,maxcached=33,maxshared=33,maxconnections=2)
